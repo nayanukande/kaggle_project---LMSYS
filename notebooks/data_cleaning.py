b1c7e8cbc2f7e4675466f9cb6f27ce7874ae1e77
Nayan Ukande <nayanukande@gmail.com>
2024-08-02 20:43:42 +0530

    cleaned all code snippet indicating their specific task.

--- notebooks/data_cleaning.py
+++ notebooks/data_cleaning.py
@@ -1,43 +1,21 @@
-# Python file to cleaning dataset
-
-# Importing all necessary library
 import pandas as pd
 import numpy as np
 import re
+import codecs
 
-# Loading the dataset
-data=pd.read_csv(r'C:\Users\nayan\test_conda\kaggle_competition\data\external\train.csv')
-df=pd.DataFrame(data)
-
-# Define the cleaning function
-def remove_unwanted_characters(text):
-    return re.sub(r'[^\w\s]', '', text)
-
-# Apply the cleaning function to relevant columns
-df['clean_prompt'] = df['prompt'].apply(remove_unwanted_characters)
-df['clean_response_a'] = df['response_a'].apply(remove_unwanted_characters)
-df['clean_response_b'] = df['response_b'].apply(remove_unwanted_characters)
-
-# Because of cleaninng unwanted character from the given dataset we also have to remove the original columns
-columns_to_remove = ['prompt', 'response_a', 'response_b']
-df=df.drop(columns=columns_to_remove)
-
-# to change the posiiton of the column as the original once
-new_column_order = ['id', 'model_a', 'model_b', 'clean_prompt', 'clean_response_a', 'clean_response_b', 'winner_model_a', 'winner_model_b', 'winner_tie']
-df = df[new_column_order]
-
-# Function to detect Cyrilic text in a dataset
-def contains_cyrilic(text):
-    cyrillic_pattern = re.compile('[\u0400-\u04FF\u0500-\u052F\u2DE0-\u2DFF\uA640-\uA69F\u1C80-\u1C8F]')
-    return bool(cyrillic_pattern.search(text))
-# Applying the function to the dataframe
-df['contains_cyrillic'] = df['clean_response_a'].apply(lambda x: contains_cyrilic(str(x)))
-# Filter rows containing Cyrillic text
-cyrillic_rows = df[df['contains_cyrillic']]
-# To calculate the count of contains cyrillic script
-true_count = df['contains_cyrillic'].sum()
-print(true_count)
-
-df.head(10)
+# Loading the dataset(by defeault )
+df=pd.read_csv(r'C:\Users\nayan\test_conda\kaggle_competition\data\external\train.csv')
+# Identify columns that start with 'Unnamed'
+unnamed_columns = [col for col in df.columns if col.startswith('Unnamed')]
+# Drop the unwanted columns
+df.drop(columns=unnamed_columns, inplace=True)
+# Function to detect Unicode escape sequence
+def contains_unicode_escape(text):
+    if isinstance(text, str):
+        unicode_escape_pattern = re.compile(r'\\u[0-9A-Fa-f]{4}')
+        return bool(unicode_escape_pattern.search(text))
+    return False
+# Applying this function to response_a column to get the count of UES
+df.to_csv('decoded_output_file_v2.csv', index=False, encoding='utf-8-sig')
 
 
